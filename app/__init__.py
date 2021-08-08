c16f8b205cda812f065c2eb54579f92e3cc6d682
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-08 13:11:37 +0300

    registering the blurprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from config import config_options
 
-app = Flask(__name__)
+# Application factory function
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
 
 from app import views
