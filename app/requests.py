0b23ba12c7c8dbd9a3ce1b2b477dcab48c25f035
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-12 13:24:40 +0300

    modifies the url request method to get request method

--- app/requests.py
+++ app/requests.py
@@ -1,55 +1,30 @@
-import urllib.request, json
-from .models import NewsSource, Articles
+import json
+from requests import get
+from .models import Top_headlines, Article
 
-# getting api key
-api_key = None
+def get_news_objects(api_key, endpoint, base_url, query_str):
+    req_url = base_url.format(endpoint,api_key,query_str)
+    response = get(req_url)
+    news_data = json.loads(response.text)
+    articles = news_data('articles')
 
-# getting news base url
-base_url = None
+    return articles
 
-# top headlines url
-top_headlines_url = None
+def the_news_objects(article_list):
+    news_object = []
+    for item in article_list:
+        title = item.get('title')
+        source = item.get('source')
+        urlToImage = item.get('urlToImage')
+        description = item.get('description')
+        author = item.get('author')
+        publishedAt = item.get('publishedAt')
+        url = item.get('url')
+        content = item.get('content')
 
-def configure_requests(app):
-    global api_key, base_url
-    api_key = app.config['API_KEY']
-    base_url = app.config['NEWS_SOURCE_URL']
-    top_headlines_url = app.config['TOP_HEADLINES_URL']
-
-
-def get_news_source():
-    '''
-    function to getting the source data in json format to the url request
-    '''
-    get_news_source = base_url.format(api_key)
-
-    with urllib.request.urlopen(get_news_source) as url:
-        get_news_source_data = url.read()
-        get_news_source_response = json.loads(get_news_source_data)
-
-        news_source_results = None
-
-        if get_news_source_response['news_sources']:
-            news_source_results_list = get_news_source_response['news_sources']
-            news_source_results = process_results(news_source_results_list)
-
-    return news_source_results
-
-def process_results(news_source_list):
-    '''
-    The function to process results and convert them to a list of objects
-    Args:
-        news_source_list: dictionary with news source details
-    Returns:
-        news_source_results: A list of news source objects
-    '''
-    news_source_results =[]
-    for news_source_item in news_source_list:
-        id = news_source_item.get('id')
-        name = news_source_item.get('name')
-
-        if id:
-            news_source_object = NewsSource(id, name)
-            news_source_results.append(news_source_object)
+        obj = Article(title, source, urlToImage, description, author, publishedAt, url, content)
+        the_news_objects.append(obj)
     
-    return news_source_results
+    return the_news_objects
+
+
