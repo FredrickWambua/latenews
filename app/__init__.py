4e2d71a3a57d7e6a0b9f14b9a74f2ebecb3b77ae
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-12 13:26:05 +0300

    modifies path importation

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+
+from app import app
+from flask import Flask, app
+from config import config_options
+
+# Application factory function
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+
+    return app
